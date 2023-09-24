666ae3af626bff1baf9576b3f0d469513a1e6507
salex <ShutovAlexandr@yandex.ru>
2023-09-24 23:32:06 +0300

    Добавлен бинарный поиск. Превышение по памяти. Завален 5й тест.

--- src/main/java/com/salex/year2019/AlarmClocks/AlarmClocksMain.java
+++ src/main/java/com/salex/year2019/AlarmClocks/AlarmClocksMain.java
@@ -6,12 +6,14 @@ import java.util.*;
 
 public class AlarmClocksMain {
 
+    private static int n, x, k;
+
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("src/main/java/com/salex/year2019/AlarmClocks/input.txt");
         Scanner scanner = new Scanner(file);
-        int n = scanner.nextInt(); // количество будильников
-        int x = scanner.nextInt(); // периодичность звонков
-        int k = scanner.nextInt(); // количество будильников, которое нужно отключить, чтобы Алексей проснулся
+        n = scanner.nextInt(); // количество будильников
+        x = scanner.nextInt(); // периодичность звонков
+        k = scanner.nextInt(); // количество будильников, которое нужно отключить, чтобы Алексей проснулся
         Queue<Integer> t = new PriorityQueue<>(); // моменты времени на которые были заведены будильники
         Map<Integer, Integer> groups = new HashMap<>();
         for (int i = 0; i < n; i++) {
@@ -29,14 +31,29 @@ public class AlarmClocksMain {
             t.add(entry.getValue());
         }
 
-        int result = 0;
-        for (int i = 0; i < k; i++) {
-            result = t.poll();
-            Integer nextTime = result + x;
-            t.add(nextTime);
-        }
+        int result = binSearch(0, k * x, groups);
 
-        System.out.println(result);
+        System.out.println(result - x);
+    }
+
+    private static int binSearch(int l, int r, Map<Integer, Integer> map) {
+
+        int K = 0;
+        int T = l + (r - l)/2;
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+            K += countK(T, e.getValue());
+        }
+        if (K == k) {
+            return T;
+        } else if (K > k) {
+            return binSearch(l, T, map);
+        } else {
+            return binSearch(T, r, map);
+        }
+    }
+
+    private static int countK(int T, int t) {
+        return Math.max(0, (T - t)/x);
     }
 
 }
