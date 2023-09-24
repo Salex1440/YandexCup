d0877cdbb04c668a836e139a1952e84d2216154e
salex <ShutovAlexandr@yandex.ru>
2023-09-24 23:35:10 +0300

    Добавлен бинарный поиск. Превышение по памяти. Завален 5й тест.

--- src/main/java/com/salex/year2019/AlarmClocks/AlarmClocksMain.java
+++ src/main/java/com/salex/year2019/AlarmClocks/AlarmClocksMain.java
@@ -7,6 +7,7 @@ import java.util.*;
 public class AlarmClocksMain {
 
     private static int n, x, k;
+    private static Map<Integer, Integer> map = new HashMap<>();
 
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("src/main/java/com/salex/year2019/AlarmClocks/input.txt");
@@ -14,29 +15,24 @@ public class AlarmClocksMain {
         n = scanner.nextInt(); // количество будильников
         x = scanner.nextInt(); // периодичность звонков
         k = scanner.nextInt(); // количество будильников, которое нужно отключить, чтобы Алексей проснулся
-        Queue<Integer> t = new PriorityQueue<>(); // моменты времени на которые были заведены будильники
-        Map<Integer, Integer> groups = new HashMap<>();
         for (int i = 0; i < n; i++) {
             int time = scanner.nextInt();
             int key = time % x;
-            if (!groups.containsKey(key)) {
-               groups.put(key, time);
+            if (!map.containsKey(key)) {
+               map.put(key, time);
            } else {
-                Integer value = groups.get(key);
+                Integer value = map.get(key);
                 value = value < time ? value : time;
-                groups.put(key, value);
+                map.put(key, value);
             }
         }
-        for (Map.Entry<Integer, Integer> entry : groups.entrySet()) {
-            t.add(entry.getValue());
-        }
 
-        int result = binSearch(0, k * x, groups);
+        int result = binSearch(0, k * x);
 
         System.out.println(result - x);
     }
 
-    private static int binSearch(int l, int r, Map<Integer, Integer> map) {
+    private static int binSearch(int l, int r) {
 
         int K = 0;
         int T = l + (r - l)/2;
@@ -46,9 +42,9 @@ public class AlarmClocksMain {
         if (K == k) {
             return T;
         } else if (K > k) {
-            return binSearch(l, T, map);
+            return binSearch(l, T);
         } else {
-            return binSearch(T, r, map);
+            return binSearch(T, r);
         }
     }
 
