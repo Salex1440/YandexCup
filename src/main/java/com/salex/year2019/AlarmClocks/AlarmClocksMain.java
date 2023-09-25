90d14f9190ecddfd96ac79c4b04f5a595904279a
salex <ShutovAlexandr@yandex.ru>
2023-09-25 16:35:37 +0300

    Проведены оптимизации. 5й тест почти укладывается в 2 секунды.

--- src/main/java/com/salex/year2019/AlarmClocks/AlarmClocksMain.java
+++ src/main/java/com/salex/year2019/AlarmClocks/AlarmClocksMain.java
@@ -1,7 +1,5 @@
 package com.salex.year2019.AlarmClocks;
 
-import com.sun.xml.internal.fastinfoset.util.CharArray;
-
 import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -23,16 +21,17 @@ public class AlarmClocksMain {
             int ch;
             CharArrayWriter caw = new CharArrayWriter();
             for (int i = 0; i < n; i++) {
-                while ((ch = br.read()) != -1) {
-                    if (ch == 32 || ch == 10) break;
+                ch = br.read();
+                while (!(ch == -1 || ch == 32 || ch == 10)) {
                     caw.append((char)ch);
+                    ch = br.read();
                 }
                 time = Long.parseLong(caw.toString());
                 caw.reset();
                 key = time % x;
                 if (map.containsKey(key)) {
                     value = map.get(key);
-                    map.put(key, Math.min(value, time));
+                    map.put(key, (value < time) ? value : time);
                 } else {
                     map.put(key, time);
                 }
@@ -40,13 +39,14 @@ public class AlarmClocksMain {
         }
 
         long K, T = 0;
-        long l = 0, r = k * x;
+        long l = 0, r = k * x, temp = 0;
         Collection<Long> values = map.values();
         while (l < r) {
             K = 0;
             T = l + (r - l) / 2;
             for (Long t : values) {
-                K += Math.max(0, (T - t) / x);
+                temp = (T - t) / x;
+                K += (0 >= temp) ? 0 : temp;
             }
             if (K == k) {
                 break;
