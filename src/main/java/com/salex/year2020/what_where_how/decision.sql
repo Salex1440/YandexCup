9ce114ee713349205bdb7233de28cfde39e6887e
salex <ShutovAlexandr@yandex.ru>
2023-10-01 21:57:18 +0300

    Применены логические операторы

--- src/main/java/com/salex/year2020/what_where_how/decision.sql
+++ src/main/java/com/salex/year2020/what_where_how/decision.sql
@@ -1,6 +1,8 @@
 SELECT job_name, host_name, trigger_fire_time, job_finished_time,
-(strftime('%s', job_finished_time) - strftime('%s', trigger_fire_time)) AS duration,
-0 AS duration_diff
+IIF(job_status = 'OK',
+ (strftime('%s', job_finished_time) - strftime('%s', trigger_fire_time)), 0) AS duration,
+0 AS duration_diff,
+job_status
 FROM qrtz_log
 -- GROUP BY job_name
 -- HAVING max(trigger_fire_time)
