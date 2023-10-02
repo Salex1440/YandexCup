37a56106e106f60b814648837187725725e38743
salex <ShutovAlexandr@yandex.ru>
2023-10-02 11:20:29 +0300

    Получен ответ для колонки duration

--- src/main/java/com/salex/year2020/what_where_how/decision.sql
+++ src/main/java/com/salex/year2020/what_where_how/decision.sql
@@ -1,9 +1,22 @@
-SELECT job_name, host_name, trigger_fire_time, job_finished_time,
-IIF(job_status = 'OK',
- (strftime('%s', job_finished_time) - strftime('%s', trigger_fire_time)), 0) AS duration,
-0 AS duration_diff,
-job_status
+
+WITH HD AS (
+SELECT
+qrtz_log.host_name,
+qrtz_log.job_name AS jn,
+max(strftime('%s', job_finished_time) - strftime('%s', trigger_fire_time)) as max_host_duration
 FROM qrtz_log
--- GROUP BY job_name
--- HAVING max(trigger_fire_time)
+GROUP BY qrtz_log.host_name, qrtz_log.job_name
+)
+
+SELECT job_name, qrtz_log.host_name, trigger_fire_time, job_finished_time,
+IIF(job_status = 'OK', strftime('%s', job_finished_time) - strftime('%s', trigger_fire_time), 0) AS duration,
+-- 0 AS duration_diff,
+job_status,
+HD.max_host_duration
+FROM qrtz_log
+JOIN HD ON HD.host_name = qrtz_log.host_name
+WHERE job_status IS NOT NULL OR (strftime('%s', '2020-09-01 10:00:00') - strftime('%s', trigger_fire_time) > 2 * HD.max_host_duration)
+GROUP BY job_name
+HAVING max(trigger_fire_time)
 ;
+
