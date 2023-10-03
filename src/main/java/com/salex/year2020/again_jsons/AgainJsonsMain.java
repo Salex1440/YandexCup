cf01f225743870abbbfbc5587fd69f3aa13ff245
salex <ShutovAlexandr@yandex.ru>
2023-10-03 10:36:01 +0300

    Решение принято

--- src/main/java/com/salex/year2020/again_jsons/AgainJsonsMain.java
+++ src/main/java/com/salex/year2020/again_jsons/AgainJsonsMain.java
@@ -8,7 +8,7 @@ import org.json.simple.parser.ParseException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.*;
 
 public class AgainJsonsMain {
 
@@ -17,29 +17,21 @@ public class AgainJsonsMain {
         BufferedReader br = new BufferedReader(new FileReader(file));
         int n = Integer.parseInt(br.readLine());
 
+        JSONArray list = new JSONArray();
         JSONParser parser = new JSONParser();
         for (int i = 0; i < n; i++) {
             String json = br.readLine();
             JSONObject jsonObject = (JSONObject) parser.parse(json);
-            JSONArray msg = (JSONArray) jsonObject.get("offers");
-            Iterator<String> iterator = msg.iterator();
+            JSONArray shops = (JSONArray) jsonObject.get("offers");
+            Iterator iterator = shops.iterator();
             while (iterator.hasNext()) {
-                System.out.println(iterator.next());
+                JSONObject next = (JSONObject) iterator.next();
+                list.add(next);
             }
         }
-
-    }
-
-    private class Shop {
-        private String offerId;
-        private Long marketSku;
-        private Long price;
-
-        public Shop(String offerId, Long marketSku, Long price) {
-            this.offerId = offerId;
-            this.marketSku = marketSku;
-            this.price = price;
-        }
+        JSONObject jobj = new JSONObject();
+        jobj.put("offers", list);
+        System.out.println(jobj);
     }
 
 }
