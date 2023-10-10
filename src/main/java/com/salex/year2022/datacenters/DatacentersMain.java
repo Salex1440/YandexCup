7a5ddb29b2f7de9898598df523727a3899ff6ecf
salex <ShutovAlexandr@yandex.ru>
2023-10-10 10:28:31 +0300

    Добавлен контейнер treeMap для поиска максимального и минимального значений

--- src/main/java/com/salex/year2022/datacenters/DatacentersMain.java
+++ src/main/java/com/salex/year2022/datacenters/DatacentersMain.java
@@ -1,52 +1,70 @@
 package com.salex.year2022.datacenters;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class DatacentersMain {
 
     public static void main(String[] args) throws IOException {
         File file = new File("src/main/java/com/salex/year2022/datacenters/input.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
-        String line = br.readLine();
-        String[] split = line.split(" ");
-        int n = Integer.parseInt(split[0]); // number of datacenters
-        int m = Integer.parseInt(split[1]); // number of servers in a datacenter
-        int q = Integer.parseInt(split[2]);
+        int n = Integer.parseInt(readWord(br)); // number of datacenters
+        int m = Integer.parseInt(readWord(br)); // number of servers in a datacenter
+        int q = Integer.parseInt(readWord(br));
         int[] R = new int[n];
         int[] A = new int[n];
-        boolean[][] servers = new boolean[n][m];
+        Map<Integer, Set<Integer>> servers = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            servers.put(i, new HashSet<>());
+        }
         int[] value = new int[n];
         Arrays.fill(A, m);
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
         int i, j, maxI = 0, minI = 0;
         for (int c = 0; c < q; c++) {
             switch (readWord(br)) {
                 case "RESET":
                     i = Integer.parseInt(readWord(br)) - 1;
+                    int prev = value[i];
                     A[i] = m;
                     R[i]++;
-                    for (int k = 0; k < m; k++) {
-                        servers[i][k] = true;
+                    servers.put(i, new HashSet<>());
+                    int val = A[i] * R[i];
+                    value[i] = val;
+                    if (!treeMap.containsKey(val) || treeMap.get(val) > i) {
+                        treeMap.put(val, i);
+                        if (val != prev) {
+                            treeMap.remove(prev);
+                        }
                     }
-                    value[i] = A[i] * R[i];
                     maxI = getMaxInd(value, n);
                     minI = getMinInd(value, n);
                     break;
                 case "DISABLE":
                     i = Integer.parseInt(readWord(br)) - 1;
                     j = Integer.parseInt(readWord(br)) - 1;
-                    if (servers[i][j] == true) {
-                        servers[i][j] = false;
+                    prev = value[i];
+                    if (!servers.get(i).contains(j)) {
+                        servers.get(i).add(j);
                         A[i]--;
-                        value[i] = A[i] * R[i];
-                        maxI = getMaxInd(value, n);
+                        val = A[i] * R[i];
+                        value[i] = val;
+                        if (!treeMap.containsKey(val) || treeMap.get(val) > i) {
+                            treeMap.put(val, i);
+                            if (val != prev) {
+                                treeMap.remove(prev);
+                            }
+                        }
+                        maxI = getMaxInd(treeMap);
                         minI = getMinInd(value, n);
                     }
                     break;
                 case "GETMAX":
+                    System.out.println(getMaxInd(treeMap) + 1);
                     System.out.println(maxI + 1);
                     break;
                 case "GETMIN":
+                    System.out.println(-1);
                     System.out.println(minI + 1);
                     break;
             }
@@ -58,18 +76,35 @@ public class DatacentersMain {
         int ch;
         CharArrayWriter caw = new CharArrayWriter();
         while ((ch = br.read()) != ' ' && ch != '\n') {
-            caw.append((char)ch);
+            caw.append((char) ch);
         }
         return caw.toString();
     }
 
 
-    public static int getMaxInd(int[] value, int n) {
+    private static int getMaxInd(int[] value, int n) {
         int maxI = 0;
         for (int k = 0; k < n; k++) {
             maxI = (value[k] > value[maxI]) ? k : maxI;
         }
         return maxI;
+    }
+
+    private static int getMaxInd(TreeMap<Integer, Integer> treeMap) {
+        int maxI = 0;
+        if (treeMap.size() > 0) {
+            maxI = treeMap.lastEntry().getValue();
+        }
+        return maxI;
+    }
+
+
+    private static int getMinInd(TreeMap<Integer, Integer> treeMap) {
+        int minI = 0;
+        if (treeMap.size() > 1) {
+            minI = treeMap.firstEntry().getValue();
+        }
+        return minI;
     }
 
     public static int getMinInd(int[] value, int n) {
