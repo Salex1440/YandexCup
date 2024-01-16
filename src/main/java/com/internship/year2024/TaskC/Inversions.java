ff23c02ebe954901b8f13bc397802ae502d5e93a
salex <ShutovAlexandr@yandex.ru>
2024-01-16 16:49:47 +0300

    Реализовал подсчет инверсий с помощью сортировки слиянием. Стало проходить на 1 тест больше. 32 тест TL.

--- src/main/java/com/internship/year2024/TaskC/Inversions.java
+++ src/main/java/com/internship/year2024/TaskC/Inversions.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class Inversions {
     public static void main(String[] args) throws IOException {
-        File file = new File("src/main/java/com/internship/year2024/TaskC/input3.txt");
+        File file = new File("src/main/java/com/internship/year2024/TaskC/input.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
         int n = Integer.parseInt(readWord(br));
         int[] p = new int[n];
@@ -12,11 +12,10 @@ public class Inversions {
             p[i] = Integer.parseInt(readWord(br));
         }
         long sum = 0, cnt = 0;
-
         for (int i = 0; i < p.length - 1; i++) {
             for (int j = i + 1; j < p.length; j++) {
                 swap(p, i, j);
-                sum += countInversions(p);
+                sum += countInversions(p.clone());
                 swap(p, i, j);
                 cnt++;
             }
@@ -42,14 +41,47 @@ public class Inversions {
         p[i] = t;
     }
 
+//    private static int countInversions(int[] p) {
+//        int result = 0;
+//        for (int i = 0; i < p.length - 1; i++) {
+//            for (int j = i + 1; j < p.length; j++) {
+//                if (p[i] > p[j]) result++;
+//            }
+//        }
+//        return result;
+//    }
+
     private static int countInversions(int[] p) {
-        int result = 0;
-        for (int i = 0; i < p.length - 1; i++) {
-            for (int j = i + 1; j < p.length; j++) {
-                if (p[i] > p[j]) result++;
+        if (p.length <= 1) return 0;
+        int[] left = (p.length % 2 == 0) ? new int[p.length/2] : new int[p.length/2 + 1];
+        System.arraycopy(p, 0, left, 0, left.length);
+        int[] right = new int[p.length/2];
+        System.arraycopy(p, left.length, right, 0, right.length);
+        int leftInv = countInversions(left);
+        int rightInv = countInversions(right);
+        int splitInv = merge(left, right, p);
+        return leftInv + rightInv + splitInv;
+    }
+
+    private static int merge(int[] left, int[] right, int[] sorted) {
+        int inversions = 0;
+        int i = 0, j = 0;
+        while (i < left.length && j < right.length) {
+            int l = left[i], r = right[j];
+            if (l <= r) {
+                sorted[i + j] = left[i++];
+            } else {
+                sorted[i + j] = right[j++];
+                inversions += left.length - i;
             }
         }
-        return result;
+        while (i < left.length) {
+            sorted[i + j] = left[i++];
+        }
+        while (j < right.length) {
+            sorted[i + j] = right[j++];
+        }
+        return inversions;
     }
 
     private static String readWord(BufferedReader br) throws IOException {
