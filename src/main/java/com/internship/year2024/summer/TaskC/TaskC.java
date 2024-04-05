0c70c4a3fffe1aadbaa5b71c6738905fc52e59fb
salex <ShutovAlexandr@yandex.ru>
2024-04-05 10:33:29 +0300

    feat: Tack C. TL 10 test

--- src/main/java/com/internship/year2024/summer/TaskC/TaskC.java
+++ src/main/java/com/internship/year2024/summer/TaskC/TaskC.java
@@ -1,12 +1,37 @@
 package com.internship.year2024.summer.TaskC;
 
-import java.io.BufferedReader;
-import java.io.CharArrayWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class TaskC {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File file = new File("src/main/java/com/internship/year2024/summer/TaskC/input2.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        int n = Integer.parseInt(readWord(br));
+        int[] a = new int[n];
+        int[] b = new int[n];
+        boolean[] c = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(readWord(br));
+            b[i] = Integer.parseInt(readWord(br));
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if ((a[i] < a[j] && b[i] < b[j]) || (a[i] > a[j] && b[i] > b[j])) {
+                } else {
+                    c[i] = true;
+                    c[j] = true;
+                }
+            }
+        }
+        long result = 0;
+        for (int i = 0; i < n; i++) {
+            if (!c[i]) {
+                result++;
+            }
+        }
+        System.out.println(result);
 
     }
 
