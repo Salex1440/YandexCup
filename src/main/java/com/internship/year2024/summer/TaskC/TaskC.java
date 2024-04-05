41c9e1e2787839d9a7c1c9c5413741dad6e4d1ae
salex <ShutovAlexandr@yandex.ru>
2024-04-05 11:03:23 +0300

    feat: Tack C. TL 10 test

--- src/main/java/com/internship/year2024/summer/TaskC/TaskC.java
+++ src/main/java/com/internship/year2024/summer/TaskC/TaskC.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class TaskC {
 
     public static void main(String[] args) throws IOException {
-        File file = new File("src/main/java/com/internship/year2024/summer/TaskC/input2.txt");
+        File file = new File("src/main/java/com/internship/year2024/summer/TaskC/input1.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
         int n = Integer.parseInt(readWord(br));
         int[] a = new int[n];
@@ -16,19 +16,21 @@ public class TaskC {
             b[i] = Integer.parseInt(readWord(br));
         }
 
+        int result = n;
         for (int i = 0; i < n; i++) {
+            if (c[i]) continue;
             for (int j = i + 1; j < n; j++) {
-                if ((a[i] < a[j] && b[i] < b[j]) || (a[i] > a[j] && b[i] > b[j])) {
-                } else {
-                    c[i] = true;
-                    c[j] = true;
+//                if (i == j) continue;
+                if (!((a[i] < a[j] && b[i] < b[j]) || (a[i] > a[j] && b[i] > b[j]))) {
+                    if (!c[i]) {
+                        c[i] = true;
+                        result--;
+                    }
+                    if (!c[j]) {
+                        c[j] = true;
+                        result--;
+                    }
                 }
-            }
-        }
-        long result = 0;
-        for (int i = 0; i < n; i++) {
-            if (!c[i]) {
-                result++;
             }
         }
         System.out.println(result);
