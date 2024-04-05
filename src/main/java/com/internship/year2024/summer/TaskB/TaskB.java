9ffd868bbdfda4a52e59684ef5dc10018eb1f1a3
salex <ShutovAlexandr@yandex.ru>
2024-04-05 13:15:09 +0300

    feat: Task B. OK

--- src/main/java/com/internship/year2024/summer/TaskB/TaskB.java
+++ src/main/java/com/internship/year2024/summer/TaskB/TaskB.java
@@ -1,13 +1,52 @@
 package com.internship.year2024.summer.TaskB;
 
-import java.io.BufferedReader;
-import java.io.CharArrayWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class TaskB {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File file = new File("src/main/java/com/internship/year2024/summer/TaskB/input2.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        int n = Integer.parseInt(readWord(br));
+        int q = Integer.parseInt(readWord(br));
 
+        String[] dict = new String[n];
+        for (int i = 0; i < n; i++) {
+            dict[i] = readWord(br);
+        }
+
+        for (int i = 0; i < q; i++) {
+            int pos = Integer.parseInt(readWord(br));
+            String prefix = readWord(br);
+            int idx = findFirstPref(prefix, dict);
+            if (idx == -1) {
+                System.out.println(idx);
+            } else {
+                if ((idx + pos - 1 < dict.length) && dict[idx + pos - 1].startsWith(prefix)) {
+                    System.out.println(idx + pos);
+                } else {
+                    System.out.println(-1);
+                }
+            }
+        }
+    }
+
+    private static int findFirstPref(String pref, String[] dict) {
+        int s = 0, e = dict.length - 1;
+        int m = (s + e) / 2;
+        while (s < e) {
+            if (dict[m].compareTo(pref) < 0) {
+                s = m + 1;
+                m = (s + e) / 2;
+            } else if (dict[m].compareTo(pref) > 0) {
+                e = m;
+                m = (s + e) / 2;
+            } else {
+                return m;
+            }
+        }
+        if (dict[m].startsWith(pref)) return m;
+        return -1;
     }
 
     private static String readWord(BufferedReader br) throws IOException {
