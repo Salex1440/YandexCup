ca541b0e329ea07cfefdafa15ccd25cfe014d19b
salex <ShutovAlexandr@yandex.ru>
2024-04-15 12:04:18 +0300

    feat: TaskF OK

---
+++ src/main/java/com/tinkoff/year2024/summer/taskF/TaskF.java
@@ -0,0 +1,13 @@
+package com.tinkoff.year2024.summer.taskF;
+
+import java.io.*;
+
+public class TaskF {
+    public static void main(String[] args) throws IOException {
+        File file = new File("src/main/java/com/tinkoff/year2024/summer/taskF/input1.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
