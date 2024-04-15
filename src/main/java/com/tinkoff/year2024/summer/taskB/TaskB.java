b0300ee0eb7b548c6b915450f9f12761afbe57d5
salex <ShutovAlexandr@yandex.ru>
2024-04-15 08:43:52 +0300

    feat: TaskB OK

--- src/main/java/com/tinkoff/year2024/summer/taskB/TaskB.java
+++ src/main/java/com/tinkoff/year2024/summer/taskB/TaskB.java
@@ -4,8 +4,32 @@ import java.io.*;
 
 public class TaskB {
     public static void main(String[] args) throws IOException {
-        File file = new File("src/main/java/com/tinkoff/year2024/summer/taskB/input1.txt");
+        File file = new File("src/main/java/com/tinkoff/year2024/summer/taskB/input3.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(readWord(br));
+        int m = Integer.parseInt(readWord(br));
+        long[][] arr = new long[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Long.parseLong(readWord(br));
+            }
+        }
+
+        long[][] result = new long[m][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                result[j][n - 1 - i] = arr[i][j];
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     private static String readWord(BufferedReader br) throws IOException {
